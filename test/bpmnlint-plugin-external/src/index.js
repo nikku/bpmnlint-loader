790fb1b0fefc2c03e52bfebc8ff21c495a176a5b
Nico Rehwaldt <git_nikku@nixis.de>
2023-07-21 17:30:04 +0200

    test: verify compilation of external rules

---
+++ test/bpmnlint-plugin-external/src/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  configs: {
+    recommended: {
+      rules: {
+        'foo': 'error'
+      }
+    }
+  },
+  rules: {
+    foo: './foo'
+  }
+};
---
+++ test/bpmnlint-plugin-simple/rules/foo.js
@@ -0,0 +1,5 @@
+module.exports = function foo() {
+  return {
+    check() {}
+  };
+};
